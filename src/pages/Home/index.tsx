54284ac6225c339ae67025161de41900aedf300e
Milena <milenahikari11@gmail.com>
2020-12-03 22:05:58 -0300

    Adicionando icones e finalizando o header da home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Logo from '../../assets/img/logo-my-shop.png';
 
 import * as S from './styles'
@@ -6,9 +7,20 @@ import * as S from './styles'
 const Home: React.FC = () => {
   return <S.Container>
     <S.Header>
-      <S.ImageLogo source={Logo} />
+      <S.WrapperHeader>
+        <S.IconMenu name="align-left" size={20} color="#FFFFFF" />
+        <S.ImageLogo source={Logo} />
+        <S.IconNotification name="bell-o" size={20} color="#FFFFFF" />
+      </S.WrapperHeader>
+      <S.WrapperSearch>
+        <S.IconSearch name="search" size={20} color="#9B9B9B" />
+        <S.InputSearch
+          placeholder="What are you looking for?"
+          placeholderTextColor="#9B9B9B"
+          autoCorrect={false} />
+      </S.WrapperSearch>
     </S.Header>
-  </S.Container>;
+  </S.Container >;
 }
 
 export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,24 +1,63 @@
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 export const Container = styled.View`
   flex: 1;
   background: lightblue;
 `;
+
 export const Header = styled(LinearGradient).attrs({
   start: { x: 0, y: 0 },
   end: { x: 1, y: 0 },
   colors: ['#34283E', '#845FA2'],
 })`
+  position: relative;
   width: 100%;
   height: 110px;
   padding: 20px;
-  justify-content: center;
+`;
+
+export const WrapperHeader = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
+`;
+
+export const IconMenu = styled(FontAwesomeIcon)`
+  transform: scaleY(-1);
 `;
 
 export const ImageLogo = styled.Image`
   width: 88px;
   height: 24px;
   resizeMode: contain;
+`;
+
+export const IconNotification = styled(FontAwesomeIcon)`
+`;
+
+export const WrapperSearch = styled.View`
+  position: absolute;
+  width: 335px;
+  height: 44px;
+  top: 88px;
+  align-self: center;
+`;
+
+export const IconSearch = styled(FontAwesomeIcon)`
+  position: absolute;
+  left: 15px;
+  top: 10px;
+  z-index: 99999;
+`;
+
+export const InputSearch = styled.TextInput`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 40px;
+  background: #FFFFFF;
+  padding-left: 45px;
 `;
